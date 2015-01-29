23203ad9db34bcd42a82690bebbd0b0aeb79396e
Codective <codective@gmail.com>
2015-01-28 18:13:57 -0800

    Tweet!

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,7 @@
+class TweetsController < ApplicationController
+  def create
+    # tweet here
+    Tweet.create(body: params[:tweet][:body], user_id: current_user.id)
+    redirect_to "/"
+  end
+end
