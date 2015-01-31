e1a4eb9feb90e1376dbecf3102799bfbd946eb06
Codective <codective@gmail.com>
2015-01-30 17:44:29 -0800

    UI tuneup + gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def gravatar
+    Gravatar.new(email).image_url(d: :identicon)
+  end
 end
